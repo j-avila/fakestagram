d66420f9ec5dbca84c2b6bd0fd3135b6c47424c3
jlavila13 <jose.leon.avila@gmail.com>
2020-01-06 08:18:26 -0300

    listo el proceso de registro en firebase

--- store/sagas/saga.js
+++ store/sagas/saga.js
@@ -1,48 +1,33 @@
 import {takeEvery, call} from 'redux-saga/effects'
 import {authService, dataBaseService} from '../servicios/firebase'
 
-const handleRegister = data => {
+const handleRegister = data => 
 	authService.createUserWithEmailAndPassword(data.email, data.password)
-	.then( response => {
-		const resp = response.user.toJSON()
-		console.log(resp)
-		return resp
-	})
-}
+	.then( response => response.user)
 
-const saveUser = () => {
-	dataBaseService.ref('users/' + userId).set({
-		username: name,
-		email: email,
-		profile_picture : imageUrl
-	});
-}
 
-const testPromise = new Promise((resolve, reject) => {
-	setTimeout(function(){
-    resolve("¡Éxito!"); // ¡Todo salió bien!
-  }, 50000);
-}) 
-
-const testFunction = () => {
-	testPromise.then( (success) => {
-		return console.log("lo hizo?" + success)
-	})
-}
+// const saveUser = () => {
+// 	dataBaseService.ref('users/' + userId).set({
+// 		username: name,
+// 		email: email,
+// 		profile_picture : imageUrl
+// 	});
+// }
 
 function* registerService(values) {
 	try {
-		console.log("init")
-		const register =  yield call(testFunction)
+		console.log('init') 
+		const register =  yield call(handleRegister, values.payload)
 		console.log(register)
 		// console.log(values)
 		// const {userid, email} = register
-		// const {name:{name}} = values 
+		// const {name:{name}} = values
 		// yield call(saveUser, {uid, email, name})
+		console.log('end')
 	} catch (error) {
 		console.log("error: ",error)		
-	}
-} 
+	} 
+}
 
 export function* defaultSaga(values) {
 	// yield 
