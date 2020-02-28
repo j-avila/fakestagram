03dc7e92a24f22abfd2b822a99e9e084b79a0f00
jlavila13 <jose.leon.avila@gmail.com>
2020-02-28 08:09:40 -0300

    wip en comentarios

--- components/user/comments.js
+++ components/user/comments.js
@@ -1,21 +1,24 @@
 import React from 'React'
 import { View, StyleSheet, Text } from 'react-native'
+import { useSelector } from 'react-redux'
 
 const Comments = props => {
+  const user = useSelector(state => state.sessionHandler.email)
 
-	return(
-		<View style={styles.body}>
-			<Text>comments screen</Text>
-		</View>
-	)
+  console.log(user)
+  return (
+    <View style={styles.body}>
+      <Text>comments screen {user}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	body: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
 
 export default Comments
