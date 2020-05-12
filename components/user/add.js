d0347483d36445f82705492aed6b4303f9f83403
jlavila13 <jose.leon.avila@gmail.com>
2020-05-12 00:52:30 -0400

    [createPost] avance en la nueva vista

--- components/user/add.js
+++ components/user/add.js
@@ -1,36 +1,116 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  Text,
+  StyleSheet,
+  View,
+  Button,
+  KeyboardAvoidingView
+} from 'react-native'
 import { TouchableHighlight } from 'react-native-gesture-handler'
+import {
+  SET_POST_PHOTO,
+  REMOVE_POST_PHOTO,
+  CREATE_POST
+} from '../../store/actions/types'
+import PostForm from './postForm'
+import CameraPicker from './cameraPickerExpo'
+import ImagePicker from '../shared/imagePicker'
+import { useSelector, useDispatch } from 'react-redux'
+import { getPosts, setPostImg, setCreatePost } from '../../store/actions'
 
-export default class Add extends Component {
-  render() {
-    const {navigation} = this.props
-    return (
-      <View style={styles.body}>
-        <Text>add</Text>
-        <Button
-          title="take a photo"
-          onPress={ () => { navigation.navigate('Selection')}}
-        />
-       <TouchableHighlight style={styles.textTouch} onPress={() => {navigation.navigate('Selection', {
-          gallery: true
-        }
-       )}}>
-        <Text>or select from gallery</Text>
-       </TouchableHighlight>
-      </View> 
+const Add = props => {
+  const { navigation } = props
+  const dispatch = useDispatch()
+  const [type, setStype] = useState('camera')
+  const imageObj = useSelector(state => state.postImg)
+  const post = useSelector(state => state.form.createPost)
+  const postPrev = useSelector(state => state.createPost)
+  const userData = useSelector(state => state.sessionHandler)
+  const updateTimeline = dispatch(getPosts())
+
+  const setImg = image => {
+    dispatch(setPostImg(SET_POST_PHOTO, image))
+  }
+  const delImg = () => {
+    dispatch(setPostImg(REMOVE_POST_PHOTO))
+  }
+  const setPost = (post, image, userData) => {
+    dispatch(
+      setCreatePost(CREATE_POST, {
+        postDesc: post,
+        userID: userData,
+        imgUrl: image
+      })
     )
   }
+  const handleCreatePost = async (post, postPic, uid) => {
+    await setPost(post.values, postPic.image, uid)
+    navigation.navigate('Home')
+    updateTimeline()
+  }
+
+  const switchType = async type => {
+    await setStype(type)
+    imageObj.image && delImg()
+  }
+
+  return (
+    <View style={styles.body}>
+      <KeyboardAvoidingView styles={styles.holder} behavior="padding" enabled>
+        {type === 'picker' ? (
+          <>
+            <ImagePicker
+              type="text"
+              imageObj={imageObj.image}
+              action={setImg}
+              removeImg={delImg}
+            />
+            <TouchableHighlight onPress={() => delImg()}>
+              <Text>quitar imagen</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => switchType('')}>
+              <Text>usar camara</Text>
+            </TouchableHighlight>
+          </>
+        ) : (
+          <CameraPicker
+            imageObj={imageObj.image}
+            action={setImg}
+            removeImg={delImg}
+          />
+        )}
+        {imageObj.image ? (
+          <PostForm
+            image={imageObj}
+            post={post}
+            uid={userData.uid}
+            handleSubmit={handleCreatePost}
+          />
+        ) : (
+          <View>
+            <TouchableHighlight onPress={() => switchType('picker')}>
+              <Text>elegir de la galeria</Text>
+            </TouchableHighlight>
+          </View>
+        )}
+      </KeyboardAvoidingView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   body: {
+    flex: 3,
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  holder: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#e0e3d4'
-  },
-  textTouch: {
-    marginVertical: 20
+    alignContent: 'center',
+    backgroundColor: '#FFFFFF'
   }
 })
+
+export default Add
