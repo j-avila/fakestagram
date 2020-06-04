d25867ef487aa610a1067c536876c7b34b83c60b
j-avila <jose.leon.avila@gmail.com>
2020-06-04 01:24:37 -0400

    layout de busqueda terminada

--- components/user/searchIndex.js
+++ components/user/searchIndex.js
@@ -1,39 +1,61 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import Avatar from '../shared/avatar'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler'
 
 const PorfileItem = props => {
   return (
-    <View style={styles.profile}>
+    <TouchableOpacity style={styles.profile}>
       <Avatar />
       <Text>user</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
+const dumb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 const SearchIndex = () => {
+  const [loading, setLoading] = useState(false)
+
+  const fetchUsers = load => {
+    setTimeout(() => {
+      setLoading(!load)
+    }, 10000)
+  }
+
   return (
-    <View style={{ felx: 1, justifyContent: 'space-between' }}>
-      <Text> heres goes search</Text>
+    <ScrollView>
+      <Text style={styles.title}>Resultados</Text>
       <View style={styles.container}>
-        <PorfileItem />
-        <PorfileItem />
+        {dumb.length >= 1 ? (
+          <FlatList
+            data={dumb}
+            refreshing={loading}
+            renderItem={() => <PorfileItem />}
+            onRefresh={() => fetchUsers(loading)}
+          />
+        ) : (
+          <Text>nothing to show</Text>
+        )}
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
 export default SearchIndex
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-around'
+  title: {
+    color: 'grey',
+    fontSize: 13,
+    padding: 10
   },
+  container: {},
   profile: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
+    paddingVertical: 14,
+    marginBottom: 8,
+    alignItems: 'center',
+    flexDirection: 'row'
   }
 })
